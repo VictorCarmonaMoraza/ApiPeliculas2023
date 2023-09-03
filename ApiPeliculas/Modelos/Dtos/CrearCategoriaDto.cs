292b25045e48b29126f26a341e30c09f806d3d77
Victor <VictorCarmona9035@hotmail.com>
2023-09-03 12:29:44 +0200

    11- DTO para Categoria (19)

---
+++ ApiPeliculas/Modelos/Dtos/CrearCategoriaDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos.Dtos
+{
+    public class CrearCategoriaDto
+    {
+        //Esta validacion es importante sino se crea vacia el nombre de categoria
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(60, ErrorMessage = "El numero maximo de caracteres es de 100!")]
+        public string Nombre { get; set; }
+    }
+}
