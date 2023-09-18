97723ddea8ab1c01712a5ed30350b6c7d4dc9fb9
Victor <VictorCarmona9035@hotmail.com>
2023-09-18 19:32:15 +0200

    13-Controlador Categorias(21)

---
+++ ApiPeliculas/Controllers/CategoriasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPeliculas.Controllers
+{
+    [ApiController]
+    //Opcion 1 => [Route("api/[controller]")]
+    //Opcion 2 =>[Route("api/categoria")] =>esta es mejor porque si cambia el nombre del controlador seguira funcionando
+    [Route("api/categoria")]
+    public class CategoriasController : ControllerBase
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
