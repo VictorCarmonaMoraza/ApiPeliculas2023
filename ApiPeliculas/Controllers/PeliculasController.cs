9ebbe4ef1e24ab824b5fcbf22395cc36398b8a4d
Victor <VictorCarmona9035@hotmail.com>
2023-09-20 20:38:24 +0200

    23-Pelicula Individual (32)

--- ApiPeliculas/Controllers/PeliculasController.cs
+++ ApiPeliculas/Controllers/PeliculasController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiPeliculas.Controllers
 {
-    //[Route("api/[controller]")]
     [Route("api/peliculas")]
     [ApiController]
     public class PeliculasController : ControllerBase
@@ -22,20 +21,17 @@ namespace ApiPeliculas.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Obtenemos listado de peliculas
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult GetPeliculass()
         {
-            //Forma 1
             IEnumerable<Pelicula> listaPeliculas = _pelRepo.GetPeliculas().ToList();
-            //Forma 2
-            //var listaPeliculas2 = _pelRepo.GetPeliculas().ToList();
-
-            //Forma 3
-            //var listaPeliculas3 = from pelicula in _pelRepo.GetPeliculas() select pelicula;
-
-            //
+         
             var listaPeliculaDto = new List<PeliculaDto>();
 
             foreach (var item in listaPeliculas)
@@ -43,6 +39,33 @@ namespace ApiPeliculas.Controllers
                 listaPeliculaDto.Add(_mapper.Map<PeliculaDto>(item));
             }
             return Ok(listaPeliculaDto);
+        }
+
+        /// <summary>
+        /// Obtenemos pelicula por su id
+        /// </summary>
+        /// <param name="peliculaId">id de la pelicula</param>
+        /// <returns></returns>
+        [HttpGet("{peliculaId:int}", Name = "GetPelicula")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetPelicula(int peliculaId)
+        {
+            var peliculaInBBDD = _pelRepo.GetPelicula(peliculaId);
+
+            //Comprobamos que la pelicula no es nula
+            if (peliculaInBBDD == null)
+            {
+                return BadRequest($"No encontramos la pelicula para el id: {peliculaId}");
+            }
+
+            //Mapeamos la pelicula con mapper
+            var itemPeliculaDto = _mapper.Map<PeliculaDto>(peliculaInBBDD);
+
+            //devolvemos la pelicula
+            return Ok(itemPeliculaDto);
         }
     }
 }
